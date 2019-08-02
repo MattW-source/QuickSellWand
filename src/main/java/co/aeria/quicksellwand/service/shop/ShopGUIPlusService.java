745056d8bcb1577a3e9f96eb141e3d0d7f3ad315
DNx5 <dnxnim@gmail.com>
2019-08-02 23:00:13 +0700

    Properly use ShopGui+ API.

--- src/main/java/co/aeria/quicksellwand/service/shop/ShopGUIPlusService.java
+++ src/main/java/co/aeria/quicksellwand/service/shop/ShopGUIPlusService.java
@@ -8,9 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 import net.brcdev.shopgui.ShopGuiPlugin;
 import net.brcdev.shopgui.ShopGuiPlusApi;
-import net.brcdev.shopgui.player.PlayerData;
-import net.brcdev.shopgui.shop.Shop;
-import net.brcdev.shopgui.shop.ShopItem;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
@@ -25,22 +22,17 @@ public class ShopGUIPlusService implements ShopService {
 
     @Override
     public SellResult sell(Player player, List<ItemStack> items) {
-        PlayerData playerData = shopPlugin.getPlayerManager().getPlayerData(player);
         List<ItemStack> soldItems = new ArrayList<>();
         double soldPrice = 0;
         SellResultType resultType = SellResultType.NO_ITEM_SOLD;
         for (ItemStack item : items) {
             try {
-                ShopItem shopItem = ShopGuiPlusApi.getItemStackShopItem(player, item);
-                if (shopItem != null) {
-                    Shop shop = ShopGuiPlusApi.getItemStackShop(player, item);
-                    double sellPrice = shopItem.getSellPriceForAmount(shop, player, playerData, item.getAmount());
-                    if (sellPrice > 0) {
-                        shopPlugin.getEconomyProvider().deposit(player, sellPrice);
-                        soldItems.add(item);
-                        soldPrice += sellPrice;
-                        resultType = SellResultType.SOLD;
-                    }
+                double sellPrice = ShopGuiPlusApi.getItemStackPriceSell(player, item);
+                if (sellPrice > 0) {
+                    shopPlugin.getEconomyProvider().deposit(player, sellPrice);
+                    soldItems.add(item);
+                    soldPrice += sellPrice;
+                    resultType = SellResultType.SOLD;
                 }
             } catch (Exception ex) {
                 ex.printStackTrace();
